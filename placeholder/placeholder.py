2949ee7882b0a52ed4cd49fa5cb5592eb8a87b31
hilarionq <hilarionq@gmail.com>
2017-08-09 12:10:38 -0400

    placeholder: Adding caching.

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import hashlib
 from io import BytesIO
 
 from PIL import Image, ImageDraw
@@ -8,6 +9,8 @@ from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
 from django import forms
+from django.core.cache import cache
+from django.views.decorators.http import etag
 
 DEBUG =  os.environ.get('DEBUG', 'on') == 'on'
 
@@ -37,19 +40,28 @@ class ImageForm(forms.Form):
         """ Generate an image of the given type and return as raw bytes. """
         height = self.cleaned_data['height']
         width = self.cleaned_data['width']
-        image = Image.new('RGB', (width, height))
-        draw = ImageDraw.Draw(image)
-        text = '{} X {}'.format(width, height)
-        textwidth, textheight = draw.textsize(text)
-        if textwidth < width and textheight < height:
-            texttop = (height - textheight) // 2
-            textleft = (width - textwidth) // 2
-            draw.text((textleft, texttop), text, fill=(255, 255, 255))
-        content = BytesIO()
-        image.save(content, image_format)
-        content.seek(0)
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} X {}'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
         return content
 
+def generate_etag(request, width, height):
+    content = 'Placeholder: {0} x {1}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+@etag(generate_etag)
 def placeholder(request, width, height):
     form = ImageForm({'height': height, 'width': width})
     if form.is_valid():
